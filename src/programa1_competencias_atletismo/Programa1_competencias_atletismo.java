6986cb4df416c6bce62290864363d37e6e0199b3
Dominic Casares Aguirre <dominiccasares10@gmail.com>
2022-09-29 19:34:07 -0600

    Inicio

---
+++ src/programa1_competencias_atletismo/Programa1_competencias_atletismo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa1_competencias_atletismo;
+
+/**
+ *
+ * @author domin
+ */
+public class Programa1_competencias_atletismo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
